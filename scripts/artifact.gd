782496d679365cbe213369ad17feeef73893e617
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-03 23:19:15 -0400

    Dragging an artifact raises it up to a cap

--- scripts/artifact.gd
+++ scripts/artifact.gd
@@ -1,5 +1,9 @@
 class_name Artifact extends RigidBody3D
 
+# Constants
+var UP_FORCE = 2.5 # The constant number used for the y axis when being dragged so that the artifact goes up
+var Y_CAP = 1 # The max Y position to apply the upward force to
+
 # Properties
 var dragging = false
 
@@ -16,7 +20,7 @@ func _physics_process(delta: float) -> void:
 	var obj_screen_pos = grave_camera.unproject_position(self.global_transform.origin)
 	var mouse_pos = get_viewport().get_mouse_position()
 	var direction_vector_screen = mouse_pos - obj_screen_pos
-	var direction_vector_world = Vector3(direction_vector_screen.x, 0, direction_vector_screen.y)
+	var direction_vector_world = Vector3(direction_vector_screen.x, UP_FORCE if global_position.y < Y_CAP else 0, direction_vector_screen.y)
 	
 	# Apply the force
 	apply_central_impulse(direction_vector_world.normalized())

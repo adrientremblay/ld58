9e46bceb95c16ea1a146cb769dc02304ebc4ea8f
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-04 11:33:44 -0400

    Basics of the artifact labels is working

--- scenes/hud.gd
+++ scenes/hud.gd
@@ -2,6 +2,7 @@ extends Control
 
 # Children
 @onready var money_label: Label = $AnchorTopRight/VBoxContainer/MoneyLabel
+@onready var artifact_panel: PanelContainer = $ArtifactPanel
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,3 +15,11 @@ func _process(delta: float) -> void:
 func update_money_label(money: float):
 	# calculate the value
 	money_label.text = "Money: " + str(money) + "$"
+
+func _on_grave_move_artifact_label_to_cursor(artifact: Artifact) -> void:
+	fill_artifact_container(artifact)
+	artifact_panel.position = get_viewport().get_mouse_position()
+
+func fill_artifact_container(artifact: Artifact):
+	$ArtifactPanel/VBoxContainer/NameLabel.text = artifact.name
+	$ArtifactPanel/VBoxContainer/ValueLabel.text = "Value: " + str(artifact.value) + "$"
--- scripts/grave.gd
+++ scripts/grave.gd
@@ -12,6 +12,7 @@ var RAY_LENGTH = 500
 
 # Signals
 signal collect_artifact(artifact: Artifact)
+signal move_artifact_label_to_cursor(artifact: Artifact)
 
 func _ready() -> void:
 	sack_model.visible = false
@@ -20,16 +21,22 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("click") and grave_active:
 		pickup_item()
 
+func _unhandled_input(event: InputEvent) -> void:
+	if grave_active and event is InputEventMouseMotion:
+		Screen.print("mouse moved")
+		# Casting the ray
+		var result = shoot_ray()
+		# Checking for a match with an artifact
+		if result:
+			if result.collider.is_in_group("artifact"):
+				var artifact: Artifact = result.collider
+				if artifact.dragging:
+					return
+				move_artifact_label_to_cursor.emit(artifact)
+
 func pickup_item():
-	# Determine what a raycast vector pointing straight down should be on top of the cursor
-	var mouse_position = get_viewport().get_mouse_position()
-	var ray_origin = grave_camera.project_ray_origin(mouse_position)
-	var ray_target = ray_origin + grave_camera.project_ray_normal(mouse_position) * RAY_LENGTH
-	# Creating the raycast
-	var space = get_world_3d().direct_space_state
-	var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_target, 2)
 	# Casting the ray
-	var result = space.intersect_ray(query)
+	var result = shoot_ray()
 	# Checking for a match with an artifact
 	if result:
 		if result.collider.is_in_group("artifact"):
@@ -53,3 +60,15 @@ func _on_sack_detection_area_body_entered(body: Node3D) -> void:
 		var artifact: Artifact = body
 		collect_artifact.emit(artifact)
 		artifact.queue_free()
+
+func shoot_ray() -> Dictionary:
+	# Determine what a raycast vector pointing straight down should be on top of the cursor
+	var mouse_position = get_viewport().get_mouse_position()
+	var ray_origin = grave_camera.project_ray_origin(mouse_position)
+	var ray_target = ray_origin + grave_camera.project_ray_normal(mouse_position) * RAY_LENGTH
+	# Creating the raycast
+	var space = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_target, 2)
+	# Casting the ray
+	var result = space.intersect_ray(query)
+	return result

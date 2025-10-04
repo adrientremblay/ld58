c6f5ba81744004e64c85cbe0a59a9a621e9a2fc9
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-04 16:49:29 -0400

    Fixed corruption

--- scripts/tool.gd
+++ scripts/tool.gd
@@ -24,21 +24,15 @@ func _physics_process(delta: float) -> void:
 	var grave_camera = get_viewport().get_camera_3d()
 	
 	# if the object's y position is near the Y cap, set it to the Y Cap
-	if global_position.y > Y_CAP - 0.1:
-		self.linear_velocity = Vector3(0,0,0)
-		var mouse_position = get_viewport().get_mouse_position()
-		var camera_origin = grave_camera.project_ray_origin(mouse_position)
-		var target_direction = (grave_camera.project_ray_normal(mouse_position)).normalized()
-		var t = (Y_CAP - camera_origin.y) / target_direction.y
-		global_position = camera_origin + target_direction * t
-	else:
-		# Determine vector to apply force 
-		var obj_screen_pos = grave_camera.unproject_position(self.global_transform.origin)
-		var mouse_pos = get_viewport().get_mouse_position()
-		var direction_vector_screen = mouse_pos - obj_screen_pos
-		var direction_vector_world = Vector3(direction_vector_screen.x, UP_FORCE if global_position.y < Y_CAP else 0, direction_vector_screen.y)
-			
-		# Apply the force
-		apply_central_impulse(direction_vector_world.normalized())
+	var mouse_pos = get_viewport().get_mouse_position()
+	var camera_origin = grave_camera.project_ray_origin(mouse_pos)
+	var ray_dir = grave_camera.project_ray_normal(mouse_pos)
+	var t = (Y_CAP - camera_origin.y) / ray_dir.y
+	var target_pos = camera_origin + ray_dir * t
+
+	global_position = global_position.lerp(target_pos, 0.2)
+	
+	if global_position.y > Y_CAP - 0.2:
+		global_position.y = Y_CAP
 		
 		

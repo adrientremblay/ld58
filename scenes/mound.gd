e14218a0d20d6748c4fe0d41d3c91752ea2ac2a5
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-04 13:57:56 -0400

    Basic version of the shovel tool working

--- scenes/mound.gd
+++ scenes/mound.gd
@@ -1,10 +1,25 @@
-extends Node3D
+class_name Mound extends Node3D
 
+# Children
+@onready var mound_top = $MoundTop
+@onready var mound_mid = $MoundMid
+@onready var mound_bottom = $MoundBottom
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var area_top: Area3D = $TopArea
+@onready var area_mid: Area3D  = $MidArea
+@onready var area_bottom: Area3D  = $BottomArea
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_top_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group("tool") and mound_top:
+		mound_top.queue_free()
+		area_top.monitoring = false
+
+func _on_mid_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group("tool") and mound_mid:
+		mound_mid.queue_free()
+		area_mid.monitoring = false
+
+func _on_bottom_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group("tool") and mound_top:
+		mound_bottom.queue_free()
+		area_bottom.monitoring = false

feadda471d8fb89747e87cdf0107eae1b1fd6b36
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-05 18:32:12 -0400

    Gave collection screen a title

--- scenes/hud.gd
+++ scenes/hud.gd
@@ -31,7 +31,7 @@ func _on_grave_move_artifact_label_to_cursor(artifact: Artifact) -> void:
 
 func fill_artifact_container(artifact: Artifact):
 	$ArtifactPanel/VBoxContainer/NameLabel.text = artifact.artifact_name
-	$ArtifactPanel/VBoxContainer/ValueLabel.text = "Value: " + str(artifact.value) + "$"
+	$ArtifactPanel/VBoxContainer/ValueLabel.text = "Collection Value: " + str(artifact.value) + "$"
 
 func _on_player_player_can_interact(interact_message: String) -> void:
 	interact_label.text = interact_message

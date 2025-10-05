8c9e70b6ac9fcb28983161a5c1baf9ed178112de
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-05 12:52:10 -0400

    Basic groundskeeper chasing the player AI

--- scripts/groundskeeper.gd
+++ scripts/groundskeeper.gd
@@ -2,12 +2,14 @@ extends CharacterBody3D
 
 # Enums
 enum Mode {
-	IDLING,
-	PATROLLING,
+	IDLING, # standing in place
+	PATROLLING, # walking between patrol nodes
+	CHASING, # chasing the player
 }
 
 # Exports
 @export var patrol_node: PatrolNode
+@export var player: Player
 
 # Children
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -18,28 +20,41 @@ enum Mode {
 # Properties
 var current_mode: Mode = Mode.PATROLLING
 
+# Constants
+var RUN_MULTIPLIER = 4.0
+
 func _ready() -> void:
 	animation_player.play("Walk")
 	footstep_animation_player.play("walk_slow")
 
 func _physics_process(delta: float) -> void:
+	if current_mode == Mode.IDLING:
+		return
+	
+	var new_velocity: Vector3 = Vector3.ZERO
 	if current_mode == Mode.PATROLLING:
-		var direction = (patrol_node.global_position - global_position)
+		var direction = patrol_node.global_position - global_position
 		direction.y = 0
-		
 		if direction.length() < 0.01:
 			current_mode = Mode.IDLING
 			idle_timer.start()
 			animation_player.play("Idle")
 			footstep_animation_player.stop()
 			return
-		
-		self.velocity = direction.normalized()
-		look_at(self.global_position + direction)
-		move_and_slide()
+		direction = direction.normalized()
 		footstep_animation_player.play("walk_slow")
+		new_velocity = direction
+	elif current_mode == Mode.CHASING:
+		var direction = (player.global_position - global_position)
+		direction.y = 0
+		direction = direction.normalized()
+		new_velocity = direction * RUN_MULTIPLIER
+		footstep_animation_player.play("walk")
+		
+	self.velocity = new_velocity
+	look_at(self.global_position + velocity)
+	move_and_slide()
 	
-
 func _on_idle_timer_timeout() -> void:
 	patrol_node = patrol_node.next
 	current_mode = Mode.PATROLLING
@@ -47,8 +62,11 @@ func _on_idle_timer_timeout() -> void:
 	footstep_animation_player.play("walk_slow")
 	idle_timer.stop()
 
-		
 func play_random_footstep():
 	var footstep_sounds_list = footstep_sounds.get_children()
 	var random_sound = footstep_sounds_list[randi_range(0, footstep_sounds_list.size()-1)]
 	random_sound.play()
+
+func _on_player_detection_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		current_mode = Mode.CHASING

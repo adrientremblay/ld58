8ac9377c34d7167d5d5fedc0eff991b0b73990ee
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-05 10:26:09 -0400

    Added check for landing at node

--- scripts/groundskeeper.gd
+++ scripts/groundskeeper.gd
@@ -18,7 +18,12 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var direction = (patrol_node.global_position - global_position)
 	direction.y = 0
+	
+	if direction.length() < 0.01:
+		Screen.print("at node")
+	
 	self.velocity = direction.normalized()
 	look_at(self.global_position + direction)
 	move_and_slide()
+	
 	

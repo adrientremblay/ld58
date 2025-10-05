a0f25d98b50950088ce46e147db473e5a616cfed
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-05 10:23:18 -0400

    Groundskeeper doesn't look at floor

--- scripts/groundskeeper.gd
+++ scripts/groundskeeper.gd
@@ -16,8 +16,9 @@ func _ready() -> void:
 	animation_player.play("Walk")
 
 func _physics_process(delta: float) -> void:
-	var direction = (patrol_node.global_position - global_position).normalized()
-	self.velocity = direction
-	look_at(patrol_node.global_position)
+	var direction = (patrol_node.global_position - global_position)
+	direction.y = 0
+	self.velocity = direction.normalized()
+	look_at(self.global_position + direction)
 	move_and_slide()
 	

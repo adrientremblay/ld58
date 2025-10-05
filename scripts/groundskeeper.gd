27a02180bd2b97ddc03ff6e6b757cff0e5636499
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-05 10:32:38 -0400

    groundskeeperr patrol works

--- scripts/groundskeeper.gd
+++ scripts/groundskeeper.gd
@@ -11,19 +11,31 @@ enum Mode {
 
 # Children
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var idle_timer: Timer = $IdleTimer
+
+# Properties
+var current_mode: Mode = Mode.PATROLLING
 
 func _ready() -> void:
 	animation_player.play("Walk")
 
 func _physics_process(delta: float) -> void:
-	var direction = (patrol_node.global_position - global_position)
-	direction.y = 0
+	if current_mode == Mode.PATROLLING:
+		var direction = (patrol_node.global_position - global_position)
+		direction.y = 0
+		
+		if direction.length() < 0.01:
+			current_mode = Mode.IDLING
+			idle_timer.start()
+			animation_player.play("Idle")
+		
+		self.velocity = direction.normalized()
+		look_at(self.global_position + direction)
+		move_and_slide()
 	
-	if direction.length() < 0.01:
-		Screen.print("at node")
-	
-	self.velocity = direction.normalized()
-	look_at(self.global_position + direction)
-	move_and_slide()
-	
-	
+
+func _on_idle_timer_timeout() -> void:
+	patrol_node = patrol_node.next
+	current_mode = Mode.PATROLLING
+	animation_player.play("Walk")
+	idle_timer.stop()

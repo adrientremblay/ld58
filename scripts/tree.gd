4004ee306ffc8812731a269701d0040d0f545c45
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-05 23:29:01 -0400

    Placed trees around

--- scripts/tree.gd
+++ scripts/tree.gd
@@ -2,3 +2,5 @@ extends StaticBody3D
 
 func _ready() -> void:
 	self.rotate(Vector3.UP, randf_range(0, 360))
+	var random_scale = randf_range(0.9, 1.1)
+	self.scale = Vector3(random_scale,random_scale,random_scale)

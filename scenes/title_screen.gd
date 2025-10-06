ddaae5db43e2885f6f18fd7758a3d21bf2c1fa1b
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-06 11:21:30 -0400

    Main menu is working

--- scenes/title_screen.gd
+++ scenes/title_screen.gd
@@ -1,5 +1,27 @@
 extends Node3D
 
 func _ready() -> void:
-	$Groundskeeper/AnimationPlayer.play("Idle")
+	$Groundskeeper/AnimationPlayer.play("Idle") # TODO fix this
 	$Coffin/AnimationPlayer.play("open")
+	
+	hide_all()
+
+func _on_lore_button_pressed() -> void:
+	hide_all()
+	$AnchorCenter/Lore.visible = true
+
+func _on_instructions_button_pressed() -> void:
+	hide_all()
+	$AnchorCenter/Instructions.visible = true
+
+func _on_controls_button_pressed() -> void:
+	hide_all()
+	$AnchorCenter/Controls.visible = true
+
+func _on_start_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func hide_all():
+	$AnchorCenter/Lore.visible = false
+	$AnchorCenter/Instructions.visible = false
+	$AnchorCenter/Controls.visible = false

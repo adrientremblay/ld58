bd9854853ad54d0cfd3131b6944f8295c5779007
Adrien Tremblay <adrien@adrientremblay.com>
2025-10-06 17:23:27 -0400

    ESC quits the game

--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -9,3 +9,7 @@ func _ready() -> void:
 		end_game_label.text = "You escaped the graveyard with your stolen loot"
 	
 	score_label.text = "You stole "+str(Global.score)+"$ worth of artifacts"
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("quit"):
+		get_tree().quit()
